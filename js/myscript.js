188f409c1c18d444517990a795f11a6edba38678
CCrocilla <cla.crocilla@gmail.com>
2022-05-10 23:33:49 +0000

    Initial Commit

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+// $(document).ready(function () {
+//     $("h2").css("text-decoration", "underline"); //underline all <h2> elements
+//     $("ul").css("border", "solid 1px #ccc"); //add a border to all lists
+// });
+
+//Or
+$(document).ready(function () {
+    $("h2").addClass("underline"); //underline all <h2> elements using class
+    $("ul").addClass("border"); //add a border to all lists using class
+});
